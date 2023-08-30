9292f5fe2214e193189e0ce910a4b31b5c0a1bff
Haardik <hhaardik@uwaterloo.ca>
2023-08-30 14:16:11 -0400

    recovered USDC

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,333 @@
-console.log("Hello");
+import { Account, Contract, Provider, constants, ec } from "starknet";
+
+const erc20Abi = [
+  {
+    members: [
+      {
+        name: "low",
+        offset: 0,
+        type: "felt",
+      },
+      {
+        name: "high",
+        offset: 1,
+        type: "felt",
+      },
+    ],
+    name: "Uint256",
+    size: 2,
+    type: "struct",
+  },
+  {
+    inputs: [
+      {
+        name: "name",
+        type: "felt",
+      },
+      {
+        name: "symbol",
+        type: "felt",
+      },
+      {
+        name: "governance_account_address",
+        type: "felt",
+      },
+    ],
+    name: "constructor",
+    outputs: [],
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "governance",
+    outputs: [
+      {
+        name: "governance",
+        type: "felt",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "permittedMinter",
+    outputs: [
+      {
+        name: "minter",
+        type: "felt",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        name: "name",
+        type: "felt",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        name: "symbol",
+        type: "felt",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        name: "totalSupply",
+        type: "Uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        name: "decimals",
+        type: "felt",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        name: "account",
+        type: "felt",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        name: "balance",
+        type: "Uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        name: "owner",
+        type: "felt",
+      },
+      {
+        name: "spender",
+        type: "felt",
+      },
+    ],
+    name: "allowance",
+    outputs: [
+      {
+        name: "remaining",
+        type: "Uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        name: "minter_address",
+        type: "felt",
+      },
+    ],
+    name: "setPermittedMinter",
+    outputs: [],
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        name: "recipient",
+        type: "felt",
+      },
+      {
+        name: "amount",
+        type: "Uint256",
+      },
+    ],
+    name: "permissionedMint",
+    outputs: [],
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        name: "account",
+        type: "felt",
+      },
+      {
+        name: "amount",
+        type: "Uint256",
+      },
+    ],
+    name: "permissionedBurn",
+    outputs: [],
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        name: "recipient",
+        type: "felt",
+      },
+      {
+        name: "amount",
+        type: "Uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        name: "success",
+        type: "felt",
+      },
+    ],
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        name: "sender",
+        type: "felt",
+      },
+      {
+        name: "recipient",
+        type: "felt",
+      },
+      {
+        name: "amount",
+        type: "Uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [
+      {
+        name: "success",
+        type: "felt",
+      },
+    ],
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        name: "spender",
+        type: "felt",
+      },
+      {
+        name: "amount",
+        type: "Uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        name: "success",
+        type: "felt",
+      },
+    ],
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        name: "spender",
+        type: "felt",
+      },
+      {
+        name: "added_value",
+        type: "Uint256",
+      },
+    ],
+    name: "increaseAllowance",
+    outputs: [
+      {
+        name: "success",
+        type: "felt",
+      },
+    ],
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        name: "spender",
+        type: "felt",
+      },
+      {
+        name: "subtracted_value",
+        type: "Uint256",
+      },
+    ],
+    name: "decreaseAllowance",
+    outputs: [
+      {
+        name: "success",
+        type: "felt",
+      },
+    ],
+    type: "function",
+  },
+];
+
+const ACCOUNT_ADDRESS = process.env.ACCOUNT_ADDRESS as string;
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
+const RECIPIENT_ADDRESS = process.env.RECIPIENT_ADDRESS as string;
+
+const ETH_TOKEN_ADDR =
+  "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
+
+const USDC_TOKEN_ADDR =
+  "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8";
+
+const provider = new Provider({
+  sequencer: {
+    baseUrl: constants.BaseUrl.SN_MAIN,
+  },
+});
+
+const account = new Account(provider, ACCOUNT_ADDRESS, PRIVATE_KEY);
+
+async function main() {
+  const usdcContract = new Contract(erc20Abi, USDC_TOKEN_ADDR, account);
+  const ethContract = new Contract(erc20Abi, ETH_TOKEN_ADDR, account);
+
+  const usdcBalance = await usdcContract.functions.balanceOf(account.address);
+  const ethBalance = await ethContract.functions.balanceOf(account.address);
+
+  console.log({ usdcBalance });
+  console.log({ ethBalance });
+
+  const usdcTransferTx = await usdcContract.transfer(
+    RECIPIENT_ADDRESS,
+    usdcBalance.balance
+  );
+
+  await provider.waitForTransaction(usdcTransferTx.transaction_hash);
+}
+
+main();
